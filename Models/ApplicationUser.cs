57831724b6915de9066d9057e9957b4004c3ea7b
NgocDiep0204 <htndiep2277@gmail.com>
2025-03-01 00:11:33 +0700

    Revert "update"
    This reverts commit ad5a6cf4c4bf7d01309e112d72ada68efdf58094.

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -5,8 +5,8 @@ namespace api.Models;
 public class ApplicationUser : IdentityUser
 {
     public string? FullName { get; set; }
-    public string? Gender { get; set; }
+    public string? Gender { get; set; } 
     public string? ImageUrl { get; set; }
-    public ICollection<Reviews>? CustomerReviews { get; set; } // Khách hàng viết đánh giá
-    public ICollection<Reviews>? Customer { get; set; } // Khách hàng viết đánh giá
+    public ICollection<Reviews>? CustomerReviews { get; set; }  // Khách hàng viết đánh giá
+   
 }
